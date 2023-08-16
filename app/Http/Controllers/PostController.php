b5174ebb05af16d98503e5cd8ac7f1a8922be5cc
FariedY <farriedy@gmail.com>
2023-08-16 14:13:59 +0700

    create & read Posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    public function index(): View
+    {
+        //get posts
+        $posts = Post::latest()->paginate(5);
+
+        //render view with posts
+        return view('posts.index', compact('posts'));
+    }
+    public function create(): View
+    {
+        return view('posts.create');
+    }
+    public function store(Request $request): RedirectResponse
+    {
+        //validate form
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+}
